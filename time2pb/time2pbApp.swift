aa71ebc0ee972376be17d8df5f9bb3c9a467ac82
joko0811 <perry_0096@docomo.ne.jp>
2022-10-29 15:13:50 +0900

    Initial Commit

---
+++ time2pb/time2pbApp.swift
@@ -0,0 +1,17 @@
+//
+//  time2pbApp.swift
+//  time2pb
+//
+//  Created by 岡野真空 on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct time2pbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
